4bbcc132a2bbbb4cff809434fefa5110340b3984
AhmedWaleed <ahmdwlyahmd09@gmail.com>
2019-03-18 18:44:27 +0200

    update tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,8 +30,8 @@ class CreateUsersTable extends Migration
             $table->string('city');                                        // The city of the user
             $table->date('joinedAt')->nullable();                                      // The date of his joining in the website
             $table->datetime('lastActive')->nullable();                                // Last active
-            $table->integer('followersCount');                             // followers count
-            $table->integer('followingCounts');                            // following count
+            $table->integer('followersCount')->nullable();                             // followers count
+            $table->integer('followingCounts')->nullable();                            // following count
             $table->float('ratingAvg');                                    // Raging average
             $table->integer('ratingCount'); 
             $table->integer('bookCount')->nullable();                                // Book count

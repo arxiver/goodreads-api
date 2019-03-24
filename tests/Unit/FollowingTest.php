4c2aefa7a952fe9b010fa074bd145db5aa95be89
AhmedWaleed <ahmdwlyahmd09@gmail.com>
2019-03-24 09:46:01 +0200

    git back the tests of mokhtar

---
+++ tests/Unit/FollowingTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class FollowingTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testFollowing()
+    {
+        /**
+         * Get a random user for authentication
+         */
+        $usersCount = User::all()->count();
+        $randomUserId = rand(1, $usersCount);
+        $user = User::find( $randomUserId);
+
+        /**
+         * Login assertion
+         */
+        $loginResponse = $this->json('POST', 'api/login', ['email' =>$user['email'], 'password' => 'password']);
+        $loginResponse->assertSee("token")->assertStatus(200);
+
+        /**
+         * Getting the token
+         */
+        $jsonArray = json_decode($loginResponse->content(),true);
+        $token = $jsonArray['token'];
+
+        /**
+         * Following assertion for GET following method
+         */
+        $response = $this->json('GET', 'api/following', [ 'token'=> $token ,'token_type' =>'bearer']);
+        $response->assertStatus(200)->assertSee('following');
+    }
+}

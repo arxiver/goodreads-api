d066505b0690ff05c1841deaf955bbeda73db0ff
AhmedWaleed <ahmdwlyahmd09@gmail.com>
2019-04-11 16:44:12 +0200

    pull from the repo and organize the api.php

--- database/factories/Comments.php
+++ database/factories/Comments.php
@@ -7,7 +7,7 @@ $factory->define(App\Comment::class, function (Faker $faker) {
     $users = \App\User::all()->pluck('id')->toArray();
     $rev = \App\Review::all()->pluck('id')->toArray();
     return [
-        'user_id' =>$faker->randomElement($users),
+        'user_id' =>$faker->numberBetween(1,2),
         'resourse_id'=>$faker->randomElement($rev),                  
         'resourse_type'=>0,        
         'body'=>Str::random(20),
--- database/factories/genre.php
+++ database/factories/genre.php
@@ -5,7 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\Genre::class, function (Faker $faker) {
     $books = \App\Book::all()->pluck('id')->toArray();
     return [
-        //
         'book_id'=>$faker->randomElement($books),
         'type' =>$faker->colorName,
     ];

222158bd5936e64919a3451b70333c44140484bd
AhmedWaleed <ahmdwlyahmd09@gmail.com>
2019-04-12 22:59:46 +0200

    put real data in the database

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -24,7 +24,7 @@ class UserSeeder extends Seeder
             "birthday"      => date("Y-n-j", strtotime("1998-2-21")),
             "country"       => "Canada",
             "city"          => "Atawwa",
-            "image_link"    => "default.jpg"
+            "image_link"    => "https://www.kalw.org/sites/kalw/files/styles/medium/public/201601/Nature-Brain.jpg"
         );
         User::create($Create);
 
@@ -53,7 +53,7 @@ class UserSeeder extends Seeder
             "birthday"      => date("Y-n-j", strtotime("1998-2-21")),
             "country"       => "Egypt",
             "city"          => "Cairo",
-            "image_link"    => "https://drive.google.com/open?id=1x3U1Fikm5Y_gugc0z_w9Rw_YJaa49cMD"
+            "image_link"    => "https://drive.google.com/open?id=1e4u3wVSMy5-vz_uMqETDpb6rQTroIsXq"
         );
         User::create($Create3);
 
@@ -67,7 +67,7 @@ class UserSeeder extends Seeder
             "birthday"      => date("Y-n-j", strtotime("1998-2-21")),
             "country"       => "Egypt",
             "city"          => "Cairo",
-            "image_link"    => "https://drive.google.com/open?id=1x3U1Fikm5Y_gugc0z_w9Rw_YJaa49cMD"
+            "image_link"    => "https://natureconservancy-h.assetsadobe.com/is/image/content/dam/tnc/nature/en/photos/tnc_36722630_Full.jpg?crop=0,0,6549,4912&wid=580&hei=435&scl=11.291954022988506"
         );
         User::create($Create4);
 
@@ -81,7 +81,7 @@ class UserSeeder extends Seeder
             "birthday"      => date("Y-n-j", strtotime("1998-2-21")),
             "country"       => "Egypt",
             "city"          => "Cairo",
-            "image_link"    => "https://drive.google.com/open?id=1x3U1Fikm5Y_gugc0z_w9Rw_YJaa49cMD"
+            "image_link"    => "https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/images/2018/06/edinburgh_meadows_2008_middle_meadow_walk_by_catharine_ward_thompson.jpg?itok=ysmDaSjD&fc=50,50"
         );
         User::create($Create5);
 
@@ -95,7 +95,7 @@ class UserSeeder extends Seeder
             "birthday"      => date("Y-n-j", strtotime("1998-2-21")),
             "country"       => "Egypt",
             "city"          => "Cairo",
-            "image_link"    => "https://drive.google.com/open?id=1x3U1Fikm5Y_gugc0z_w9Rw_YJaa49cMD"
+            "image_link"    => "https://drive.google.com/open?id=1BqB_oeiQcUoO2XHWMZ-ddecan6c9krL2"
         );
         User::create($Create6);
 
